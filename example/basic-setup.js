5cfd1484b4ddfe702a9c624757747f100af48748
Emily Rose <nexxy@symphonysubconscious.com>
2015-04-24 13:24:38 -0700

    Moved basic example to the example directory

--- example/basic-setup.js
+++ example/basic-setup.js
@@ -1,7 +1,18 @@
 var SoftAPSetup = require('../index');
+var config = require('../config');
+var path = require('path');
+
 var sap = new SoftAPSetup();
 
+if(!config.get('ssid')) {
+
+	console.log("* Please specify the ssid of the AP with which you wish to connect your device...");
+	console.log("Example: %s %s --ssid BestWiFiNetworkEver --password SuperSecretPassword --security wpa2_mixed", process.argv[0], path.relative(process.cwd(), __filename));
+	process.exit(1);
+}
+
 console.log("Obtaining device information...");
+
 sap.deviceInfo(claim);
 
 function claim(err, dat) {
@@ -38,10 +49,10 @@ function configure(err, dat) {
 	console.log("Scanned APs. Configuring device...");
 
 	sap.configure({
-		ssid: 'test'
-		, channel: 11
-		, password: 'testtest'
-		, security: 'wpa2_mixed'
+		ssid: config.get('ssid')
+		, channel: config.get('channel') || 11
+		, password: config.get('password') || undefined
+		, security: config.get('security') || undefined
 	}, connect);
 
 };
